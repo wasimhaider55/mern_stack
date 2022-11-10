8ef1ebd9046cca939583dcee62de57dcb31db129
wasim Haider55 <wasimhaider14379@gmail.com>
2022-11-10 15:19:56 +0500

    lec 3

---
+++ javascript/lec3rd.js
@@ -0,0 +1,46 @@
+// product. json file creted from here 
+
+const data = {
+    "products": [
+      {
+        "name": "laptop",
+        "price": 1000, 
+        "isavaiable": true
+      },
+      {
+        "name": "tv",
+        "price": 1700,
+        "isavaiable": true
+      },
+      {
+        "name": "mobile",
+        "price": 1300,
+        "isavaiable": true
+      },
+      {
+        "name": "washing machine",
+        "price": 6600,
+        "isavaiable": true
+      },
+      {
+        "name": "fridge",
+        "price": 1400,
+        "isavaiable": true
+      }
+    ]
+  }
+
+//   const products = data.products;
+//   for (let i = 0; i < products.length; i++ ){
+//     console.log(products[i].name);
+//   }
+
+
+
+  const products = data.products;
+  for (let i = 0; i < products.length; i++ ){
+    let sum = 0;
+    sum = sum +  products[i].price;
+    console.log(sum);
+  }
+
