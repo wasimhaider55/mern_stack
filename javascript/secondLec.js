51e888648a866cc62c87f29b6ce85a19b96a73a5
wasim Haider55 <wasimhaider14379@gmail.com>
2022-11-10 11:41:32 +0500

    lec 2 completed

--- javascript/secondLec.js
+++ javascript/secondLec.js
@@ -157,34 +157,34 @@
 
 
 
-// product. json file creted from here 
+// // product. json file creted from here 
 
-const data = {
-    "products": [
-      {
-        "name": "laptop",
-        "price": 1000, 
-        "isavaiable": true
-      },
-      {
-        "name": "tv",
-        "price": 10700,
-        "isavaiable": true
-      },
-      {
-        "name": "mobile",
-        "price": 1300,
-        "isavaiable": true
-      },
-      {
-        "name": "washing machine",
-        "price": 6600,
-        "isavaiable": true
-      },
-      {
-        "name": "fridge",
-        "price": 1400,
-        "isavaiable": true
-      }
-    ]
-  }
+// const data = {
+//     "products": [
+//       {
+//         "name": "laptop",
+//         "price": 1000, 
+//         "isavaiable": true
+//       },
+//       {
+//         "name": "tv",
+//         "price": 10700,
+//         "isavaiable": true
+//       },
+//       {
+//         "name": "mobile",
+//         "price": 1300,
+//         "isavaiable": true
+//       },
+//       {
+//         "name": "washing machine",
+//         "price": 6600,
+//         "isavaiable": true
+//       },
+//       {
+//         "name": "fridge",
+//         "price": 1400,
+//         "isavaiable": true
+//       }
+//     ]
+//   }
