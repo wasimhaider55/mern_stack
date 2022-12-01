3e7e0ff4a61534d592625ebfd3fb0cb1addeeb64
wasim Haider55 <wasimhaider14379@gmail.com>
2022-12-01 22:19:50 +0500

    complete modifeid html css project and add js file

---
+++ htmlCss/ecommerce/app.js
@@ -0,0 +1,26 @@
+// it's jsut for paratice 
+
+
+
+var element ;
+//  element = document;
+//  element = document.all;
+//  element = document.all[9];
+//  element = document.all[6];
+//  element = document.head;
+//  element = document.title;
+//  element = document.links;
+//  element = document.links[6];
+//  element = document.images;
+// element = document.images[2];
+//  element = document.forms;
+// element = document.URL;
+//  element = document.domain;
+//  element = document.baseURI;
+// element = document.getElementById("search");
+//  element = document.getElementsByClassName("manu");
+//  element = document.getElementsByClassName("manu")[1];
+// element = document.getElementsByTagName("ul");
+ element = document.getElementsByTagName("ul")[1];
+
+console.log(element);
