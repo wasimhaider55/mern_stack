d4385e2366e50b725c71fcccf7600513e331d41f
wasim Haider55 <wasimhaider14379@gmail.com>
2022-12-08 22:18:49 +0500

    module is done of yahoo baba

---
+++ jsYahooBaba/modules/library.js
@@ -0,0 +1,35 @@
+
+
+
+
+
+// export default function(){
+//     console.log("this is default function ");
+// }
+
+
+
+
+
+
+
+
+//  let message = "Es6 javaScript";
+
+//  function user(name){
+//     console.log(`Hellow   ${name} `)
+// }
+
+
+//  class test{
+//     constructor(){
+//         console.log("this is constructor");
+//     }
+// }
+
+// export {message , user, test }
+
+
+
+
+// * agreegaid module
---
+++ jsYahooBaba/modules/main.js
@@ -0,0 +1,31 @@
+
+
+// // import {default as wasim} from "./library.js" // we  alise the name of defualt function 
+// import wasim from "./library.js" // you can also write like that 
+
+// wasim();
+
+
+
+
+
+
+
+
+// import { message ,user ,test } from "./library.js";
+
+// // printing variable 
+// console.log(message);
+
+
+
+// // printing functions 
+// user("wasim Haider");
+// user("Asim Haider");
+
+
+// // printing classes 
+// let a = new test();
+
+
+// // document.body.innerHTML = message; //  shwoing to dom
