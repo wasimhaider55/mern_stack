d68b7250e730c7802e0693a80a654b7a4dc820f8
wasim Haider55 <wasimhaider14379@gmail.com>
2022-12-26 18:16:52 +0500

    react lec first completed

---
+++ react/basics/components/class/jsx/hello/src/Home.js
@@ -0,0 +1,18 @@
+import { Component } from 'react';
+import './Home.css';
+class Home extends Component {
+
+
+    // render function return jsx this is the wrok of render function 
+    render() {
+
+        return (
+            <div className='Home'>
+                <p >this is the para of react </p>
+                <button>click</button>
+            </div>
+        );
+    }
+
+}
+export default Home;
