a1970c8168c8f13c768c067570e912fd09346111
wasim Haider55 <wasimhaider14379@gmail.com>
2022-12-26 15:38:49 +0500

    creating first react app

---
+++ react/basics/components/class/jsx/hello/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <p>this is my first react App</p>
+    </div>
+  );
+}
+
+export default App;
