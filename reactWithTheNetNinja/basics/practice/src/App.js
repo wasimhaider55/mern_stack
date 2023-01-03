eb1e07110c1f16490eacc67fa96d3065d69aef02
wasim Haider55 <wasimhaider14379@gmail.com>
2023-01-03 14:28:33 +0500

    done 6 lec of theNetNinja

--- reactWithTheNetNinja/basics/practice/src/App.js
+++ reactWithTheNetNinja/basics/practice/src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+ const  title = "welcome to the new blog";
+ const likes = 50;
+//  const person = {name : "wasim", id: 20}; // we cannot dynamic object and boolen 
+const link = "http://www.google.com";
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <div className="contnet">
+      <h1>{title}</h1>
+      <p>likes {likes} times </p>
+      {/* <p>{person}</p> // we cannot dynamic object and boolen */}
+      <p>{10}</p>
+      <p>{"wasim Haider"}</p>
+      <p>{ [1,2,3,5,7,8,5]}</p>
+      <p>{Math.floor( Math.random() * 10 )}</p>
+      <a href={link}>Google site</a>
+
+     </div>
     </div>
   );
 }
